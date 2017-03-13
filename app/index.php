9e4c3c285d29bfa982915010c66b46e29bb08c82
svatic <yuferoff.sviat@yandex.ru>
2017-03-13 22:25:03 +0500

    css

---
+++ app/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<meta charset="utf-8">
+
+	<title>Главная</title>
+	<meta name="description" content="">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<meta property="og:image" content="path/to/image.jpg">
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+	<link rel="stylesheet" href="css/main.min.css">
+
+	<!-- Chrome, Firefox OS and Opera -->
+	<meta name="theme-color" content="#000">
+	<!-- Windows Phone -->
+	<meta name="msapplication-navbutton-color" content="#000">
+	<!-- iOS Safari -->
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-2"><h1 class="logo"><a href="">Toilet</a></h1></div>
+			</div>
+				<div class="main-header">
+								<div class="col-lg-4 col-md-5 col-sm-6">
+									<div class="row">
+										<div class="nav">
+											<ul>
+												<li><a href="#">Главная</a></li>
+												<li><a href="#">Друзья</a></li>
+												<li><a href="#">Кабинки</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-2 col-lg-offset-6 col-md-3 col-md-offset-4 col-sm-4 col-sm-offset-2">
+									<div class="row">
+										<div class="profile" id="profile-butt">
+											<div class="profile-name">Касапий</div>
+											<img src="img/maksim-gorkii-aleksei-peshkov-pisatel-portret.jpg" alt="" class="profile-img">
+											<div class="dots">
+												<div class="dot"></div>
+												<div class="dot"></div>
+												<div class="dot"></div>
+											</div>
+										</div>
+										<div class="profile-drop-menu" id="profile-drop-menu">
+											<div class="drop-butt"><a href="#">Профиль</a></div>
+											<div class="drop-butt"><a href="#">Настройки</a></div>
+											<div class="drop-butt"><a href="#">Редактировать</a></div>
+											<div class="drop-butt"><a href="#">Выход</a></div>
+										</div>
+									</div>
+								</div>
+						</div>
+				</div>
+				<div class="tablet-menu">
+					<div class="tb-profile">
+						<img src="img/maksim-gorkii-aleksei-peshkov-pisatel-portret.jpg" alt="" class="profile-img">
+						<div class="use-name">Касапий Бенедикт</div>
+						<div class="use-id">@kaysap</div>
+					</div>
+					<ul class="nav">
+						<li><a href="#"><span class="l-icon pr"></span>Профиль</a></li>
+						<li><a href="#"><span class="l-icon ms"></span>Сообщения</a></li>
+						<li><a href="#"><span class="l-icon fr"></span>Друзья</a></li>
+						<li><a href="#"><span class="l-icon sl"></span>Кабинки</a></li>
+						<li><a href="#"><span class="l-icon st"></span>Настройки</a></li>
+						<li><a href="#"><span class="l-icon ex"></span>Выход</a></li>
+					</li>
+				</div>
+	</header>
+	
+	<script src="js/scripts.min.js"></script>
+</body>
+</html>
--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
-
-	// Custom JS
+	$("#profile-butt").on("click",function(){
+            $('#profile-drop-menu').slideToggle(50);
+			$(this).toggleClass("profile-active");
+        });
+	
+	$(document).on('click', function(e) {
+  	if (!$(e.target).closest("#profile-butt").length) {
+    $('#profile-drop-menu').slideUp(50);
+	$("#profile-butt").removeClass("profile-active");
+    }
+    e.stopPropagation();
+    });
 
 });
